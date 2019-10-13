4851c0a7be381b04570d7e8cbfaac145ef8fa11f
Rondinelle Silva <rondinellepessoa@gmail.com>
2019-10-13 14:44:29 +0100

    Acrescentando comentários aos posts

--- src/main/java/com/rondinellepessoa/config/Instatiation.java
+++ src/main/java/com/rondinellepessoa/config/Instatiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.rondinellepessoa.domain.Post;
 import com.rondinellepessoa.domain.User;
 import com.rondinellepessoa.dto.AuthorDTO;
+import com.rondinellepessoa.dto.CommentDTO;
 import com.rondinellepessoa.repository.PostRepository;
 import com.rondinellepessoa.repository.UserRepository;
 
@@ -46,6 +47,13 @@ public class Instatiation implements CommandLineRunner{
 		
 		Post post1 = new Post(1L, sdf.parse("21/09/1983"), "Partiu viagem", "Vou viajar para Portugal, abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(2L, sdf.parse("23/09/1983"), "Bom dia", "Acordei feliz hojes!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/09/2019"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("21/09/2019"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("21/09/2019"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
